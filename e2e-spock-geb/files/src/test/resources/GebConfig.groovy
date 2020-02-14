b262fbddd0f03fb3f4119d39207bef54e1bec769
zxBCN Rodriguez_Cornejo <hector.rodriguez_cornejo.ext@boehringer-ingelheim.com>
2020-02-14 10:38:52 +0100

    Add proxy configuration

--- e2e-spock-geb/files/src/test/resources/GebConfig.groovy
+++ e2e-spock-geb/files/src/test/resources/GebConfig.groovy
@@ -1,5 +1,6 @@
 import com.gargoylesoftware.htmlunit.BrowserVersion;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.Proxy;
 
 // Load application.properties
 def properties = new Properties()
@@ -7,8 +8,36 @@ this.getClass().getResource( '/application.properties' ).withInputStream {
     properties.load(it)
 }
 
-// Selenium driver (true in constructor enables JavaScript)
-driver = { new HtmlUnitDriver(BrowserVersion.CHROME, true) }
+// Selenium driver (True in constructor to use JavaScript)
+driver = { 
+    HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true) 
+    
+    def env = System.getenv()
+    if(env.HTTP_PROXY) {
+        Proxy proxy = new Proxy();
+        URL url = new URL(env.HTTP_PROXY);
+        proxy.setHttpProxy("${url.getHost()}:${url.getPort()}"); 
+        proxy.setNoProxy(env.NO_PROXY)
+        driver.setProxySettings(proxy);
+    }
+    
+    return driver
+}
+
+waiting {
+    timeout = 25
+    retryInterval = 0.5
+    includeCauseInMessage = true
+    presets {
+        slow {
+            timeout = 50
+            retryInterval = 1
+        }
+        quick {
+            timeout = 10
+        }
+    }
+}
 
 // Base URL of the application to test
 baseUrl = properties."config.application.url"
