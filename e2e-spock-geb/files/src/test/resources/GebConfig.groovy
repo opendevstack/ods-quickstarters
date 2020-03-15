441824f2ae07dee6e9a7224f00249946502954ce
Martin Etmajer <martin.etmajer@boehringer-ingelheim.com>
2020-03-15 09:49:30 +0100

    Inject OpenShift project and cluster public host into env (#156)

--- e2e-spock-geb/files/src/test/resources/GebConfig.groovy
+++ e2e-spock-geb/files/src/test/resources/GebConfig.groovy
@@ -1,26 +1,23 @@
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
-import org.openqa.selenium.Proxy;
+import com.gargoylesoftware.htmlunit.BrowserVersion
+import org.openqa.selenium.htmlunit.HtmlUnitDriver
+import org.openqa.selenium.Proxy
 
 // Load application.properties
-def properties = new Properties()
-this.getClass().getResource( '/application.properties' ).withInputStream {
-    properties.load(it)
-}
+def properties = new SpecHelper().getApplicationProperties()
 
 // Selenium driver (True in constructor to use JavaScript)
-driver = { 
-    HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true) 
-    
+driver = {
+    HtmlUnitDriver driver = new HtmlUnitDriver(BrowserVersion.CHROME, true)
+
     def env = System.getenv()
     if(env.HTTP_PROXY) {
         Proxy proxy = new Proxy();
         URL url = new URL(env.HTTP_PROXY);
-        proxy.setHttpProxy("${url.getHost()}:${url.getPort()}"); 
+        proxy.setHttpProxy("${url.getHost()}:${url.getPort()}");
         proxy.setNoProxy(env.NO_PROXY)
         driver.setProxySettings(proxy);
     }
-    
+
     return driver
 }
 
