4080f5a3af282d9d0e089637bc91cef1c41d1353
Eren,Sinan (BI X) BIX-DE-I <sinan.eren@boehringer-ingelheim.com>
2020-06-16 21:41:51 +0200

    Add response message to root

--- be-typescript-express/files/src/index.ts
+++ be-typescript-express/files/src/index.ts
@@ -2,6 +2,10 @@ var express = require("express")
 
 const app = express();
 
+app.get("/", (req: any, res: any) => {
+    res.send("Hello world!");
+});
+
 app.listen(3000, () => {
   console.log(`Server is listening on port 3000!`);
 });
