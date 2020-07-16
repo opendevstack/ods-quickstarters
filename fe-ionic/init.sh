14837a412ebabb1a5fb4cfb367b65ae2686bcdcd
Manuel Feller <manuel.feller@boehringer-ingelheim.com>
2020-07-16 09:50:15 +0200

    updating ionic quickstarter to @ionic/cli

--- fe-ionic/init.sh
+++ fe-ionic/init.sh
@@ -18,9 +18,48 @@ while [[ "$#" > 0 ]]; do case $1 in
 esac; shift; done
 
 echo "generate project"
-ionic start $COMPONENT blank --type=angular --no-deps --no-git
+ionic start $COMPONENT blank --type=angular --no-deps --no-git --no-interactive
 
 cd $COMPONENT
+
+echo "Change test setup to single run in karma.conf.js"
+sed -i "s|\s*singleRun: false|singleRun: true|" ./karma.conf.js 
+
+echo "Configure headless chrome in karma.conf.js"
+read -r -d "" CHROME_CONFIG << EOM || true
+    browsers: \['ChromeNoSandboxHeadless'\],\\
+    customLaunchers: {\\
+      ChromeNoSandboxHeadless: {\\
+        base: 'Chrome',\\
+        flags: \[\\
+          '--no-sandbox',\\
+          // See https://chromium.googlesource.com/chromium/src/+/lkgr/headless/README.md\\
+          '--headless',\\
+          '--disable-gpu',\\
+          // Without a remote debugging port, Google Chrome exits immediately.\\
+          ' --remote-debugging-port=9222',\\
+        \],\\
+      },\\
+    },
+EOM
+sed -i "s|\s*browsers: \['Chrome'\],|$CHROME_CONFIG|" ./karma.conf.js
+sed -i "s|\(browsers:\)|    \1|g" ./karma.conf.js
+
+echo "Add required plugins in karma.conf.js"
+sed -i "/plugins: \[/a\     \ require('karma-junit-reporter')," ./karma.conf.js
+
+echo "Configure junit xml reporter in karma.conf.js"
+read -r -d "" UNIT_XML_CONFIG << EOM || true
+    reporters: \['progress', 'junit', 'kjhtml'\],\\
+\\
+    junitReporter: {\\
+      outputDir: './build/test-results/test',\\
+      outputFile: 'test-results.xml',\\
+      useBrowserName: false,\\
+      xmlVersion: 1\\
+    },
+EOM
+sed -i "s|\s*reporters: \['progress', 'kjhtml'\],|$UNIT_XML_CONFIG|" ./karma.conf.js
 
 echo "fix nexus repo path"
 repo_path=$(echo "$GROUP" | tr . /)
