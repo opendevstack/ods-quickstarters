080761c3df75238c40daeda5c62cea0b0ae20769
Michael Sauter <michael.sauter@boehringer-ingelheim.com>
2020-08-06 08:48:41 +0200

    Allow to run each quickstarter separately

--- tests/quickstarter-test.sh
+++ tests/quickstarter-test.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
-# set -x
-set +e
+set -eu
 set -o pipefail
+
+# By default we run all quickstarter tests, otherwise just the quickstarter
+# passed as the first argument to this script.
+QUICKSTARTER=${1-...}
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ODS_QUICKSTARTERS_DIR=${SCRIPT_DIR%/*}
@@ -20,12 +23,11 @@ if [ -f test-quickstarter-results.txt ]; then
     rm test-quickstarter-results.txt
 fi
 
-echo "Running tests ... output will take a while to arrive ..."
+echo "Running tests (${QUICKSTARTER}). Output will take a while to arrive ..."
 
-go test -v -count=1 -timeout 1h -p 4 github.com/opendevstack/ods-quickstarters/tests/... | tee test-quickstarter-results.txt 2>&1
+go test -v -count=1 -timeout 1h -p 4 github.com/opendevstack/ods-quickstarters/tests/${QUICKSTARTER} | tee test-quickstarter-results.txt 2>&1
 exitcode="${PIPESTATUS[0]}"
 if [ -f test-quickstarter-results.txt ]; then
-    set -e
     go-junit-report < test-quickstarter-results.txt > test-quickstarter-report.xml
 fi
 exit $exitcode
