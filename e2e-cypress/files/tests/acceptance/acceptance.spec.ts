cf566f6618b41bfb92cbea60d89df4218cb794aa
clemensutschig <clemens.utschig-utschig@boehringer-ingelheim.com>
2020-11-30 12:42:13 +0100

    add url promise

--- e2e-cypress/files/tests/acceptance/acceptance.spec.ts
+++ e2e-cypress/files/tests/acceptance/acceptance.spec.ts
@@ -4,7 +4,10 @@ function printEvidenceForPageElement
     throw new Error('selector must not NOT be undefined');
   }
   cy.task('log', '=====================================');
-  cy.task('log', 'Testname: ' + testName + ' // step: ' + testStep + ' // url: ' + cy.url());
+  cy.task('log', 'Testname: ' + testName + ' // step: ' + testStep);
+  cy.url().then(urlString => {
+    cy.task('log', 'URL: ' + urlString);
+  });
   cy.task('log', 'Description: ' + description);
   cy.task('log', '----- Test Evidence starts here ----');
   cy.get(selector).then(($selectedElement) => {
