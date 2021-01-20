b5d2e05d6507b52630f5ec7c66ea9aae06ff5ad4
tbugfinder <github@online.ms>
2021-01-20 10:54:09 +0100

    update random provider version and move random provider configuration to version.tf

--- inf-terraform-aws/files/random.tf
+++ inf-terraform-aws/files/random.tf
@@ -1,7 +1,3 @@
-provider "random" {
-  version = "~> 2.2.1"
-}
-
 resource "random_id" "id" {
   keepers = {
     # Create a new random ID iff the workspace name changes.
--- inf-terraform-aws/files/versions.tf
+++ inf-terraform-aws/files/versions.tf
@@ -5,9 +5,17 @@ terraform {
       source = "hashicorp/aws"
       version = "3.24.1"
     }
+    random = {
+      source = "hashicorp/random"
+      version = "3.0.1"
+    }
   }
 }
 
 provider "aws" {
+  # Configuration options
+}
+
+provider "random" {
   # Configuration options
 }
