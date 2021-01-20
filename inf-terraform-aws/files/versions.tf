e01a7282f2e387ade971dab33c28a19ab429d31a
tbugfinder <github@online.ms>
2021-01-20 10:44:21 +0100

    require terraform >=0.13. and use updated provider version locking configuration.

--- inf-terraform-aws/files/versions.tf
+++ inf-terraform-aws/files/versions.tf
@@ -1,4 +1,13 @@
-
 terraform {
-  required_version = ">= 0.12"
+  required_version = ">= 0.13"
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "3.24.1"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
 }
