f43fb73689aafee8c798a82c4478662ce281e12a
Gerard Castillo <gerard.castillo@boehringer-ingelheim.com>
2021-05-10 08:38:54 +0200

    be-nginx-gateway upgrade and maintenance for ODS4

--- be-gateway-nginx/files/docker/lua/server-error.lua
+++ be-gateway-nginx/files/docker/lua/server-error.lua
@@ -2,11 +2,16 @@ local cjson = require("cjson")
 
 local _M = {}
 
-function _M.make_error ()
-    ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+-- https://www.nginx.com/resources/wiki/extending/api/http/
+function _M.make_error (err_type)
+    if err_type == '40x' then
+        ngx.status = ngx.HTTP_BAD_REQUEST
+    else
+        ngx.status = ngx.HTTP_INTERNAL_SERVER_ERROR
+    end
     ngx.header.content_type = "application/json; charset=utf-8"
-    ngx.say(cjson.encode({ timestamp = os.time(os.date("!*t")), status = ngx.HTTP_INTERNAL_SERVER_ERROR, error = "Internal Server Error", message = "An error occurred" }))
-    return ngx.exit(ngx.HTTP_INTERNAL_SERVER_ERROR)
+    ngx.say(cjson.encode({ timestamp = os.time(os.date("!*t")), status = ngx.status }))
+    return ngx.exit(ngx.status)
 end
 
 return _M
