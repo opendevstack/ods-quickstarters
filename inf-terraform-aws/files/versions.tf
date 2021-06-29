19448879d7f8aa31dea6e8d89a573111e37cd094
tbugfinder <tbugfinder@online.ms>
2021-06-29 22:04:24 +0200

    bump terraform version from 0.14.11 to 1.0.1

--- inf-terraform-aws/files/versions.tf
+++ inf-terraform-aws/files/versions.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "3.36.0"
+      version = "3.47.0"
     }
     random = {
       source  = "hashicorp/random"
