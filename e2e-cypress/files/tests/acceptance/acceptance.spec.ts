14db55e951306c502b5f7891563d5b1093365e08
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2021-09-13 09:41:09 +0200

    change external url

--- e2e-cypress/files/tests/acceptance/acceptance.spec.ts
+++ e2e-cypress/files/tests/acceptance/acceptance.spec.ts
@@ -22,8 +22,8 @@ describe('acceptance e2e tests', function () {
   // see installation.spec.ts for examples
 
   it('Application is reachable', function () {
-    cy.visit('/html/tryit.asp?filename=tryhtml_basic_paragraphs');
-    cy.title().should('include', 'Tryit Editor v3.6');
+    cy.visit('/html/tryit.asp?filename=tryhtml_paragraphs1');
+    cy.title().should('include', 'Tryit Editor v3.7');
     printEvidenceForPageElement(
       this.test.fullTitle(), 1, '#textareaCode', 'code area');
     printEvidenceForPageElement(
