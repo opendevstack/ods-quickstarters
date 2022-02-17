f3676cfbe173f2218a02cd235984556e111b485a
s2oBCN <sergio.sacristan@boehringer-ingelheim.com>
2022-02-17 14:04:46 +0100

    improve test stability

--- e2e-spock-geb/files/src/test/acceptance/groovy/DemoAcceptanceSpec.groovy
+++ e2e-spock-geb/files/src/test/acceptance/groovy/DemoAcceptanceSpec.groovy
@@ -3,8 +3,8 @@ import geb.spock.GebReportingSpec
 import spock.lang.Stepwise
 
 class W3CAcceptanceHomePage extends Page {
-    static url = "/html/tryit.asp?filename=tryhtml_basic_paragraphs"
-    static at = { title == "Tryit Editor v3.7" }
+    static url = "/opendevstack/ods-quickstarters"
+    static at = { title.contains("quickstarters")}
 }
 
 @Stepwise
@@ -13,9 +13,9 @@ class DemoAcceptance extends GebReportingSpec {
     def "goes to W3C editor"() {
         given: "User goes to we3 editor for paragraphs and checks the content"
         to W3CAcceptanceHomePage
-        sleep (2000)
+
         // print evidence of two fields (the input area and iframe content)
-        SpecHelper.printEvidenceForPageElement(this, 1, $("#textareaCode"), "code area")
+        SpecHelper.printEvidenceForPageElement(this, 1, $("[data-content='Code']"), "code area")
         SpecHelper.printEvidenceForPageElement(this, 1, $("#iframecontainer"), "rendered code area")
 
         // print the two evidence fields through map
