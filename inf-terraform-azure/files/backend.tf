312784c0d9714d357d7e73ced2ffa63875bc2b9d
Matthias Bremser <matthias.bremser.ext@boehringer-ingelheim.com>
2022-03-07 11:30:42 +0100

    set place holders for backend

--- inf-terraform-azure/files/backend.tf
+++ inf-terraform-azure/files/backend.tf
@@ -1,7 +1,7 @@
  terraform {
   backend "azurerm" {
-    resource_group_name  = "tfstate-westeurope-dev-01-rg"
-    storage_account_name = "edpprojectdevtfstate"
+    resource_group_name  = "<resource-group>"
+    storage_account_name = "<storage-account>"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
