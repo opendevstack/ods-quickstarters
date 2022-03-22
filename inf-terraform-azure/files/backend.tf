e3d20393b663440bbc1ee7b554b19ad3796471ab
Matthias Bremser <matthias.bremser.ext@boehringer-ingelheim.com>
2022-03-22 15:33:37 +0100

    read backend config from file based on current execution environment

--- inf-terraform-azure/files/backend.tf
+++ inf-terraform-azure/files/backend.tf
@@ -1,7 +1,5 @@
  terraform {
   backend "azurerm" {
-    resource_group_name  = "<resource-group>"
-    storage_account_name = "<storage-account>"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
     use_azuread_auth     = true
