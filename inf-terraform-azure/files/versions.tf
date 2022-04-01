f57967875f0f6495343053cab995fa563be8ae15
Matthias Bremser <matthias.bremser.ext@boehringer-ingelheim.com>
2022-04-01 18:04:53 +0200

    align with skeleton

--- inf-terraform-azure/files/versions.tf
+++ inf-terraform-azure/files/versions.tf
@@ -1,10 +1,9 @@
-
 terraform {
   required_version = ">= 0.13"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "2.71.0"
+      version = "2.95.0"
     }
     local = {
       source  = "hashicorp/local"
@@ -16,7 +15,7 @@ terraform {
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.7.1"
+      version = "0.7.2"
     }
   }
 }
