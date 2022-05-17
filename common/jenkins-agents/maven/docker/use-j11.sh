1fbeb396ee6f5689557ccad6a981db168fbd7c5d
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-05-17 19:19:28 +0200

    Modification to detect that java 17 is not installed.

--- common/jenkins-agents/maven/docker/use-j11.sh
+++ common/jenkins-agents/maven/docker/use-j11.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 echo "Switching to java 11:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-11-openjdk-11.*\.x86_64" | awk '{print $NF}' | head -1) && \
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java && \
 alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac && \
 java -version
 javac -version
+
+echo "JAVA_HOME: $JAVA_HOME"
--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+
 echo "Switching to java 17:"
-exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1) && \
-alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java && \
-alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac && \
-java -version
-javac -version
+exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1)
+alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
+alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
+java -version | grep -q 17 || exit 1
+javac -version | grep -q 17 || exit 1
+
+echo "JAVA_HOME: $JAVA_HOME"
