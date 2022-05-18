730affe52c172766dfae391865f41b3d98d0b87f
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-05-18 08:26:53 +0200

    jdk 17 has no package for centos 7.

--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "JDK 17 does not work in centos 7. Sorry!!"
+exit 0
+
 echo "Switching to java 17:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1)
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
