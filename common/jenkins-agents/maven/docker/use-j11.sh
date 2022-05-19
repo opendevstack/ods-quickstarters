091a13bac4df46b4f21bf7c8ad1bbf250d239050
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-05-19 09:32:28 +0200

    javac is not there always.

--- common/jenkins-agents/maven/docker/use-j11.sh
+++ common/jenkins-agents/maven/docker/use-j11.sh
@@ -3,11 +3,12 @@
 echo "Switching to java 11:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-11-openjdk-11.*\.x86_64" | awk '{print $NF}' | head -1)
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
-alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
-
-# Check it again
 java -version | grep -q 11 || exit 1
-javac -version | grep -q 11 || exit 1
+
+if [ -x /usr/lib/jvm/${exactVersion}/bin/javac ]; then
+  alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
+  javac -version | grep -q 11 || exit 1
+fi
 
 java -version
 javac -version
--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -3,11 +3,12 @@
 echo "Switching to java 17:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1)
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
-alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
-
-# Check it again
 java -version | grep -q 17 || exit 1
-javac -version | grep -q 17 || exit 1
+
+if [ -x /usr/lib/jvm/${exactVersion}/bin/javac ]; then
+  alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
+  javac -version | grep -q 17 || exit 1
+fi
 
 java -version
 javac -version
