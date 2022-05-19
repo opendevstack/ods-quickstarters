06c46023c74b7f225884cfea59ee01fc985de502
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-05-19 10:18:45 +0200

    Fixes checking java and javac versions.

--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 
+function msg_and_exit() {
+  echo "ERROR: ${1}"
+  exit 1
+}
+
 echo "Switching to java 17:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1)
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
-java -version | grep -q 17 || exit 1
+java -version 2>&1 | grep -q 17 || msg_and_exit "Java version is not 17."
 
 if [ -x /usr/lib/jvm/${exactVersion}/bin/javac ]; then
   alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
-  javac -version | grep -q 17 || exit 1
+  javac -version 2>&1 | grep -q 17 || msg_and_exit "Javac version is not 17."
 fi
 
 java -version
-if which javac; then
+if which 'javac'; then
   javac -version
 else
-  echo "Binary javac is not available."
+  echo "WARNING: Binary javac is not available."
 fi
 echo "JAVA_HOME: $JAVA_HOME"
