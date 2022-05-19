25e6fcfcf6417d90f5106f74d6c374bb0ecfac34
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-05-19 09:11:39 +0200

    Enable switch jdk version. In ubi image they might work.

--- common/jenkins-agents/maven/docker/use-j11.sh
+++ common/jenkins-agents/maven/docker/use-j11.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
 echo "Switching to java 11:"
-exactVersion=$(ls -lah /usr/lib/jvm | grep "java-11-openjdk-11.*\.x86_64" | awk '{print $NF}' | head -1) && \
-alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java && \
-alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac && \
+exactVersion=$(ls -lah /usr/lib/jvm | grep "java-11-openjdk-11.*\.x86_64" | awk '{print $NF}' | head -1)
+alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
+alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
+
+# Check it again
+java -version | grep -q 11 || exit 1
+javac -version | grep -q 11 || exit 1
+
 java -version
 javac -version
-
 echo "JAVA_HOME: $JAVA_HOME"
--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo "JDK 17 does not work in centos 7. Sorry!!"
-exit 0
-
 echo "Switching to java 17:"
 exactVersion=$(ls -lah /usr/lib/jvm | grep "java-17-openjdk-17.*\.x86_64" | awk '{print $NF}' | head -1)
 alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || exit 1
 alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || exit 1
+
+# Check it again
 java -version | grep -q 17 || exit 1
 javac -version | grep -q 17 || exit 1
 
+java -version
+javac -version
 echo "JAVA_HOME: $JAVA_HOME"
