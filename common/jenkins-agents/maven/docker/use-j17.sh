4a0b2ebeebb5c660fa8bb77b444b88f7e376d696
Victor Pablos Ceruelo <victor.pablos_ceruelo.ext@boehringer-ingelheim.com>
2022-06-01 09:10:55 +0200

    Fixes JAVA_HOME environment variable value.

--- common/jenkins-agents/maven/docker/use-j17.sh
+++ common/jenkins-agents/maven/docker/use-j17.sh
@@ -1,24 +1,38 @@
 #!/bin/bash
 
+JAVA_HOME_FOLDER=$(ls -lah /usr/lib/jvm | grep "temurin-17" | awk '{print $NF}' | head -1)
+JAVA_VERSION="17"
+
 function msg_and_exit() {
   echo "ERROR: ${1}"
   exit 1
 }
 
-echo "Switching to java 17:"
-exactVersion=$(ls -lah /usr/lib/jvm | grep "temurin-17" | awk '{print $NF}' | head -1)
-alternatives --set java /usr/lib/jvm/${exactVersion}/bin/java || msg_and_exit "Cannot configure java 17 as the alternative to use for java."
-java -version 2>&1 | grep -q 17 || msg_and_exit "Java version is not 17."
+echo "Switching to java ${JAVA_VERSION}:"
+JAVA_HOME="/usr/lib/jvm/${JAVA_HOME_FOLDER}"
 
-if [ -x /usr/lib/jvm/${exactVersion}/bin/javac ]; then
-  alternatives --set javac /usr/lib/jvm/${exactVersion}/bin/javac || msg_and_exit "Cannot configure javac 17 as the alternative to use for javac."
-  javac -version 2>&1 | grep -q 17 || msg_and_exit "Javac version is not 17."
+alternatives --set java ${JAVA_HOME}/bin/java || \
+  msg_and_exit "Cannot configure java ${JAVA_VERSION} as the alternative to use for java."
+java -version 2>&1 | grep -q "\s\+${JAVA_VERSION}" || msg_and_exit "Java version is not ${JAVA_VERSION}."
+
+if [ -x ${JAVA_HOME}/bin/javac ]; then
+  alternatives --set javac ${JAVA_HOME}/bin/javac || \
+    msg_and_exit "Cannot configure javac ${JAVA_VERSION} as the alternative to use for javac."
+  javac -version 2>&1 | grep -q "\s\+${JAVA_VERSION}" || msg_and_exit "Javac version is not ${JAVA_VERSION}."
+else
+  echo "WARNING: Not found binary for javac in path ${JAVA_HOME}/bin/javac "
 fi
 
-java -version
+java -version 2>&1
 if which 'javac'; then
-  javac -version
+  javac -version 2>&1
 else
   echo "WARNING: Binary javac is not available."
+fi
+
+if [ -d ${JAVA_HOME}/bin/ ]; then
+  export JAVA_HOME
+else
+  msg_and_exit "Cannot configure JAVA_HOME environment variable to ${JAVA_HOME}"
 fi
 echo "JAVA_HOME: $JAVA_HOME"
