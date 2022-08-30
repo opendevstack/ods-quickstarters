12495cba98e97b7fb66d2df9d7bddc551e352852
Christian Schweikert <43915417+cschweikert@users.noreply.github.com>
2022-08-30 09:22:52 +0200

    adjust title check in cypress test
    The acceptance test for the cypress QS uses an external source for running tests against. The title field of that page seems to have changed and therfore the test is failing now. Currently it is `W3Schools Tryit Editor` without any version number in the name.
    
    BTW this reminds on my comment here https://github.com/opendevstack/ods-quickstarters/pull/502#discussion_r532600193 😜
    
    fixes #840

--- e2e-cypress/files/tests/acceptance/acceptance.spec.ts
+++ e2e-cypress/files/tests/acceptance/acceptance.spec.ts
@@ -6,7 +6,7 @@ describe('acceptance e2e tests', () => {
 
   it('Application is reachable', function () {
     cy.visit('/html/tryit.asp?filename=tryhtml_basic_paragraphs');
-    cy.title().should('include', 'Tryit Editor v3.7');
+    cy.title().should('include', 'Tryit Editor');
     printTestEvidence(this.test.fullTitle(), 1, '#textareaCode', 'code area');
     printTestEvidence(this.test.fullTitle(), 2, '#iframecontainer', 'rendered code area');
   });
