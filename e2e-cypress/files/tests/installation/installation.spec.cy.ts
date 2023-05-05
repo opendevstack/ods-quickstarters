9fe1d59842584161deb4e34355bf701fc434d232
Anna Garcia <110810707+garcanam@users.noreply.github.com>
2023-05-05 11:26:42 +0200

    Update installation.spec.cy.ts

--- e2e-cypress/files/tests/installation/installation.spec.cy.ts
+++ e2e-cypress/files/tests/installation/installation.spec.cy.ts
@@ -1,12 +1,4 @@
+/* tslint:disable:no-unused-expression */
 describe('installation e2e tests', () => {
-  it('Application is reachable and does not show error page', () => {
-    cy.visit('/html/tryit.asp?filename=tryhtml_basic_paragraphs');
-
-    // Obsolete
-    // cy.login();
-    // cy.msalv2Login();
-    // cy.visit('');
-    // cy.get('app-header').should('be.visible');
-    // cy.get('app-error-page').should('not.exist');
-  });
+  // see acceptance.spec.cy.ts for examples
 });
