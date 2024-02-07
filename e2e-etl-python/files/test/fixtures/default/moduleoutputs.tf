ee69f568ea6c2f54519b21f093c21aeeed340bf0
Your Name <cristian.perez@boehringer-ingelheim.com>
2024-02-07 20:26:57 +0100

    remove test folder remove devcontainer remove test references from the Makefile

--- e2e-etl-python/files/test/fixtures/default/moduleoutputs.tf
+++
@@ -1,5 +0,0 @@
-# This file has been created automatically.
-
-output "module_ods_quickstarters" {
-  value = module.stack-aws-quickstarter-test.*
-}
--- e2e-etl-python/files/test/fixtures/default/random.tf
+++
@@ -1,9 +0,0 @@
-provider "random" {}
-
-resource "random_id" "id" {
-  byte_length = 4
-}
-
-locals {
-  id = random_id.id.hex
-}
