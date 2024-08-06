c5c7e05eea585b51b035205eb3d3d366b90ea531
tbugfinder <github@online.ms>
2024-08-06 22:07:04 +0200

    [inf-terraform-azure] bump versions of ruby, terraform, pre-commit hooks, libraries and execute pre-commit hooks.

--- inf-terraform-azure/files/versions.tf
+++ inf-terraform-azure/files/versions.tf
@@ -3,19 +3,19 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.56.0"
+      version = "3.114.0"
     }
     local = {
       source  = "hashicorp/local"
-      version = "~> 2.4.0"
+      version = "~> 2.5.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "~> 3.5.1"
+      version = "~> 3.6.2"
     }
     time = {
       source  = "hashicorp/time"
-      version = "0.9.1"
+      version = "0.12.0"
     }
   }
 }
