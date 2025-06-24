c84fca9308eebdb1d33b90cad89889c32f350fe6
Bremser, Matthias (IT INF) EXTERNAL <matthias.bremser.ext@boehringer-ingelheim.com>
2025-06-24 11:46:32 +0000

    feat: INFIAAS-9233 Upgrade with dynamic backend configuration of Azure quickstarter

--- inf-terraform-azure/files/test/fixtures/default/main.tf
+++ inf-terraform-azure/files/test/fixtures/default/main.tf
@@ -1,5 +1,8 @@
 locals {
   name = "stack-azure-quickstarter-test"
+  tags = {
+    Name = local.name
+  }
 }
 
 module "stack-azure-quickstarter-test" {
@@ -9,4 +12,5 @@ module "stack-azure-quickstarter-test" {
   is_test          = true
   name             = local.name
   meta_environment = "DEVELOPMENT"
+  location         = "westeurope"
 }
